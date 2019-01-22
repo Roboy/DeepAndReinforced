9ef891e310845c047a8f2d2231034ec58297b176
ALPA-Industry-and-Technology <32202049+ALPA-Industry-and-Technology@users.noreply.github.com>
2019-01-22 20:21:39 +0100

    always pull the latest docker image

--- bridge_scripts/docker-simulation.sh
+++ bridge_scripts/docker-simulation.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 xhost +local:root
-docker run -v $(pwd):/root/develDeepAndReinforced -itp 11111:11311 --network=host 22ae61b3ad48
+docker run -v $(pwd):/root/develDeepAndReinforced -itp 11111:11311 --network=host deepandreinforced/rl:latest
